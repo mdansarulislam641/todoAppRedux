3adb0d11ea064edc7cfd87eed21a524e7a958ab8
mdansarulislam641 <mdansarulislam641.com>
2023-02-25 08:18:45 +0600

    project setup and component setup

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import TodoList from "./components/TodoList";
+import Footer from './components/Footer';
+function App() {
+    return (
+        <div
+        class="grid place-items-center bg-blue-100 h-screen px-6 font-sans"
+    >
+        <Navbar/>
+     
+
+        <div class="w-full max-w-3xl shadow-lg rounded-lg p-6 bg-white">
+         <Header/>
+           
+            <hr class="mt-4" />
+
+           <TodoList/>
+           
+
+            <hr class="mt-4" />
+            <Footer/>
+           
+        </div>
+    </div>
+    );
+}
+
+export default App;
